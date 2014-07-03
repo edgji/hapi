1524835be748abfae2f7566ffdd54c7e6d4e7d32
Jose Eduardo Garcia-Jimenez <j.edgji@gmail.com>
2014-07-03 16:25:34 -0700

    add pattern filtering to match entities

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -7,6 +7,13 @@ class Router extends BaseRouter {
     protected $controller;
 
     /**
+     * The entity parameter patterns.
+     *
+     * @var array
+     */
+    protected $entityPatterns = array();
+
+    /**
      * @param mixed $controller
      */
     public function setController($controller)
@@ -21,24 +28,42 @@ class Router extends BaseRouter {
      * @param  array   $options
      * @return void
      */
-    public function entityResource($model, Array $options = array())
+    public function entityResource($model, array $options = array())
     {
-        $parent = isset($options['parent']) ? str_plural($options['parent']) : false;
-        unset($options['parent']);
+        $parent = array_pull($options, 'parent', false);
+        $name = array_pull($options, 'name', false);
 
-        if (isset($options['name']))
+        if ( ! $name)
         {
-            $name = $options['name'];
-            unset($options['name']);
-        }
-        else
-        {
-            $name = ($parent) ? $parent. '/{:id}/' .str_plural($model) : str_plural($model);
+            if ($parent) {
+                $name = '{'.$parent.'}/{id}/{'.$model.'}';
+                $this->entityPattern($parent);
+            }
+            else
+            {
+                $name = '{'.$model.'}';
+            }
+            $this->entityPattern($model);
         }
 
-        $controller = isset($options['controller']) ? $options['controller'] : $this->controller;
-        unset($options['controller']);
+        //$routeKey = $this->routes->count();
 
+        $controller = array_pull($options, 'controller', $this->controller);
         $this->resource($name, $controller, $options);
+
+        //$routes = $this->routes->getRoutes();
+        //$count = count($routes);
+        //
+        //while($routeKey < $count)
+        //{
+        //    $route = $routes[$routeKey++];
+        //    $route->where($this->entityPatterns);
+        //}
+    }
+
+    protected function entityPattern($key)
+    {
+        $entity = str_plural($key);
+        $this->entityPatterns[$key] = "({$entity})";
     }
 }
