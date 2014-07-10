009d0701e19f41faa018eaaedd9d2394cd546b9c
Jose Eduardo Garcia-Jimenez <j.edgji@gmail.com>
2014-07-10 15:01:46 -0700

    implement nested resource path names

--- src/Facades/API.php
+++ src/Facades/API.php
@@ -9,8 +9,24 @@ class API extends Facade {
      * @param array $options
      * @return mixed
      */
-    public static function resource($model, Array $options = array())
+    public static function resource($model, array $options = array())
     {
-        return static::$app['router']->entityResource($model, $options);
+        return static::$app['router']->apiResource($model, $options);
+    }
+
+    /**
+     * @param string $model
+     * @param string $parent
+     * @param array $options
+     * @return mixed
+     */
+    public static function nestedResource($model, $parent = false, array $options = array())
+    {
+        if ($parent)
+        {
+            $options['parent'] = $parent;
+        }
+
+        return static::$app['router']->apiResource($model, $options);
     }
 }
--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -4,6 +4,13 @@ use Dingo\Api\Routing\Router as BaseRouter;
 
 class Router extends BaseRouter {
 
+    /**
+     * Indicates whether paths are snake cased.
+     *
+     * @var bool
+     */
+    public static $snakePaths = true;
+
     protected $controller;
 
     /**
@@ -21,17 +28,46 @@ class Router extends BaseRouter {
      * @param  array   $options
      * @return void
      */
-    public function entityResource($model, array $options = array())
+    public function apiResource($model, array $options = array())
     {
         $parent = array_pull($options, 'parent', false);
         $name = array_pull($options, 'name', false);
+        $controller = array_pull($options, 'controller', $this->controller);
 
         if ( ! $name)
         {
-            $name = ($parent) ? str_plural($parent).'/{id}/'.str_plural($model) : str_plural($model);
+            $model = (static::$snakePaths) ? snake_case(str_plural($model), '-') : str_plural(strtolower($model));
+
+            // nest resource routes if parent is present
+            if ($parent)
+            {
+                $parent = (static::$snakePaths) ? snake_case(str_plural($parent), '-') : str_plural(strtolower($parent));
+
+                $only = array_pull($options, 'only', false);
+                $except = array_pull($options, 'except', []);
+
+                $addIndexAction = (false === $only || in_array('index', $only)) && ! in_array('index', $except);
+
+                // special case resource index method needs to be handled separately here
+                if ($addIndexAction)
+                {
+                    $name = $parent . '.' . $model;
+                    $base = $model;
+
+                    $this->addResourceIndex($name, $base, $controller, $options);
+                    // make sure we skip the index action when we call the resource method
+                    $options['except'] =  array_merge($except, ['index']);
+                }
+
+                // set prefixed name for all other resource actions
+                $name = $parent . '/{'.$parent.'}/' . $model;
+            }
+            else
+            {
+                $name = $model;
+            }
         }
 
-        $controller = array_pull($options, 'controller', $this->controller);
         $this->resource($name, $controller, $options);
     }
 }
